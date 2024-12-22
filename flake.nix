cdbb1bcd6d5f8102265ee24f9031f5a9d7c2f149
Dowland Aiello <dowlandaiello@gmail.com>
2024-12-22 17:38:15 -0500

    Create project.

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      packages.${system}.default = pkgs.haskellPackages.developPackage { root = ./.; };
+    };
+}
