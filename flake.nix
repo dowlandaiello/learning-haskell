20a358ee2d6c7ba580185b7143558e02c0c10521
Dowland Aiello <dowlandaiello@gmail.com>
2024-12-22 21:55:53 -0500

    Redo project with stack only.

--- flake.nix
+++ flake.nix
@@ -1,11 +1,45 @@
 {
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in rec {
-      packages.${system}.default = pkgs.haskellPackages.developPackage { root = ./.; };
-    };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        hPkgs =
+          pkgs.haskell.packages."ghc98";
+
+        myDevTools = [
+          hPkgs.ghc
+          hPkgs.ghcid
+          hPkgs.ormolu
+          hPkgs.hlint
+          hPkgs.hoogle
+          hPkgs.haskell-language-server
+          hPkgs.implicit-hie
+          hPkgs.retrie
+          stack-wrapped
+          pkgs.zlib
+        ];
+
+        stack-wrapped = pkgs.symlinkJoin {
+          name = "stack";
+          paths = [ pkgs.stack ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/stack \
+              --add-flags "\
+                --no-nix \
+                --system-ghc \
+                --no-install-ghc \
+              "
+          '';
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = myDevTools;
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath myDevTools;
+        };
+      });
 }
